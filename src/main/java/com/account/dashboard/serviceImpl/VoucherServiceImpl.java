cd76381f242aa1ec538a1ccc7483f10166b5c068
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2025-02-27 14:02:59 +0530

    add chenges

--- src/main/java/com/account/dashboard/serviceImpl/VoucherServiceImpl.java
+++ src/main/java/com/account/dashboard/serviceImpl/VoucherServiceImpl.java
@@ -67,6 +67,7 @@ public class VoucherServiceImpl implements VoucherService{
 		Optional<Ledger> ledger = ledgerRepository.findById(createVoucherDto.getLedgerId());
 		if(ledger!=null && ledger.isPresent()&&ledger.get()!=null) {
 			v.setLedger(ledger.get());
+			v.setLedgerType(ledger.get().getLedgerType());
 		}        
 		v.setPaymentType(createVoucherDto.getPaymentType());
 
@@ -123,6 +124,8 @@ public class VoucherServiceImpl implements VoucherService{
 			map.put("paymentType", v.getPaymentType());
 			map.put("product", v.getProduct()!=null?v.getProduct().getName():"NA");
 			map.put("group", v.getLedger()!=null?v.getLedger().getLedgerType()!=null?v.getLedger().getLedgerType().getName():"NA":"");
+            map.put("voucherTypeId", v!=null?v.getLedgerType()!=null?v.getLedgerType():0:0);
+			map.put("ledgerTypeId", v.getLedgerType()!=null?v.getLedgerType().getId():0);
 
 			map.put("createDate", v.getCreateDate());
 			map.put("ledgerType", v.getLedgerType());
